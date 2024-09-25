cced4ba42057161d551397b85c35e22db6184664
Jack Formato <jpformato@loyola.edu>
2024-09-24 20:23:34 -0400

    Added my halloween costume (Jack)

--- Halloween.java
+++ Halloween.java
@@ -1,6 +1,7 @@
 public class Halloween {
     public static void main(String[] args) {
-        System.out.printf("Victoria: My favorite was a plague doctor costume.");
+        System.out.println("Victoria: My favorite was a plague doctor costume.");
 
+        System.out.println("Jack: My favorite costume was the Flash.");
     }
 }
