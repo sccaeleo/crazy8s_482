3f94829fdf0ffdc71a3189bf37adef87f0b967a0
WILLIAM GRIMMER <wpgrimmer@loyola.edu>
2024-10-15 14:39:33 -0400

    resources folder

---
+++ resources/Halloween.java
@@ -0,0 +1,9 @@
+public class Halloween {
+    public static void main(String[] args) {
+        System.out.println("Victoria: My favorite was a plague doctor costume.");
+
+        System.out.println("Jack: My favorite costume was the Flash.");
+
+	System.out.println("Will: My favorite costume was Scooby-Doo");
+    }
+}
