a49b1ac1d0f5d0b698198c0ac9061e173a269140
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2024-10-22 19:42:00 -0400

    Create Game Page (H0.1)

--- Crazy8s/client/src/pages/CreateGame.jsx
+++ Crazy8s/client/src/pages/CreateGame.jsx
@@ -4,9 +4,16 @@ import { Outlet, Link } from "react-router-dom";
 
 function CreateGame(){
     return(
+      <>
       <div>
-          <h1>Create Game</h1>
+        <h1>Create Game</h1>
       </div>
+      <div>
+        <Link to="/game">
+        <button class="btn btn-lg btn-light">Create Game</button>
+        </Link>
+      </div>
+      </>
     )
   }
   
