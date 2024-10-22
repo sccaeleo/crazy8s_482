de32ed36913bceaf3c749d410783b34ff90ded37
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2024-10-22 16:32:20 -0400

    Making main page look better (H0.2)

--- Crazy8s/client/src/pages/Home.jsx
+++ Crazy8s/client/src/pages/Home.jsx
@@ -1,26 +1,31 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Outlet, Link } from "react-router-dom";
+import './Home.css'
 
 function Homepage(){
   return(
-    <div style={{backgroundColor:"green"}}>
+    <div>
 
-    <div class=" bg-success text-white text-center">
+    <div class="text-white text-center">
         <h1><b>Crazy 8s</b></h1>
-        <Link to="/accountsettings">
-        <button class="btn btn-lg rounded-5 text-white btn-success float-end">A</button>
-        </Link>
+        <img src='logo512.png'></img>
+    </div>
+
+    <div class="account-button">
+      <Link to="/accountsettings">
+        <button class="btn btn-lg btn-light rounded-5 float-end">A</button>
+      </Link>
     </div>
     
-    <div class="text-white text-center">
+    <div class="main-buttons">
         <Link to="/create">
-        <button class="btn btn-success rounded">Create Game</button>
+        <button class="btn btn-lg btn-light">Create Game</button>
         </Link>
         <Link to="/join">
-        <button class="btn btn-success">Join Game</button>
+        <button class="btn btn-lg btn-light">Join Game</button>
         </Link>
-        <button class="btn btn-success">Tutorial</button>
+        <button class="btn btn-lg btn-light">Tutorial</button>
     </div>
     
 
