7c4f60afeda911b2df344b3ccdd4312e8ca4aac1
WillGrimmer <90355797+WillGrimmer@users.noreply.github.com>
2024-11-06 01:53:16 -0500

    forgot to write function to let you log out (A0)

--- Crazy8s/server/server.js
+++ Crazy8s/server/server.js
@@ -95,6 +95,15 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send('Error logging out');
+    }
+    res.send('User logged out');
+  });
+});
+
 
 
 app.get('/accounts', async (req, res) => {
